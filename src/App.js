6d404b799a195b654b7f0f68e5830fd27828fa41
Nadwey <81181783+Nadwey@users.noreply.github.com>
2023-05-22 23:08:59 +0200

    aaaaaaa

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import styles from "./App.module.css";
-import Autoplay from "embla-carousel-autoplay";
-import { Carousel, Embla } from "@mantine/carousel";
-import { useEffect, useRef, useState } from "react";
+import { Carousel } from "@mantine/carousel";
+import { useEffect, useState } from "react";
 import { Opinia } from "./Opinia";
-import { Flex, Group } from "@mantine/core";
+import { Group } from "@mantine/core";
 
 // images
 import tnieDrewno1 from "./img/tnie-drewno-1.jpg";
 import tnieDrewno2 from "./img/tnie-drewno-2.jpg";
-import zygmuntMalinowski1 from "./img/zygmunt-malinowski-1.png";
-import pracaDrewno1 from "./img/praca-drewno-1.jpg";
 import starosc1 from "./img/starosc-1.webp";
 import klient1 from "./img/klient-1.jpg";
 import klient2 from "./img/klient-2.jpg";
@@ -20,9 +17,6 @@ import obrobkaMetalu3 from "./img/obrobka-metalu-3.jpg";
 import obrobkaMetalu4 from "./img/obrobka-metalu-4.jpg";
 
 function App() {
-    /**
-     * @type {[Embla, null]}
-     */
     const [embla, setEmbla] = useState(null);
 
     useEffect(() => {
@@ -41,19 +35,19 @@ function App() {
             <div className={styles.carouselContainer}>
                 <Carousel loop getEmblaApi={setEmbla} draggable={false} withControls={false} height="100%" sx={{ flex: 1 }}>
                     <Carousel.Slide>
-                        <img className={styles.carouselImage} src={tnieDrewno1} />
+                        <img alt="tnie drewno" className={styles.carouselImage} src={tnieDrewno1} />
                     </Carousel.Slide>
                     <Carousel.Slide>
-                        <img className={styles.carouselImage} src={tnieDrewno2} />
+                        <img alt="tnie drewno" className={styles.carouselImage} src={tnieDrewno2} />
                     </Carousel.Slide>
                     <Carousel.Slide>
-                        <img className={styles.carouselImage} src={obrobkaMetalu2} />
+                        <img alt="obróbka metalu" className={styles.carouselImage} src={obrobkaMetalu2} />
                     </Carousel.Slide>
                     <Carousel.Slide>
-                        <img className={styles.carouselImage} src={obrobkaMetalu3} />
+                        <img alt="obróbka metalu" className={styles.carouselImage} src={obrobkaMetalu3} />
                     </Carousel.Slide>
                     <Carousel.Slide>
-                        <img className={styles.carouselImage} src={obrobkaMetalu4} />
+                        <img alt="obróbka metalu" className={styles.carouselImage} src={obrobkaMetalu4} />
                     </Carousel.Slide>
                 </Carousel>
             </div>
@@ -68,7 +62,7 @@ function App() {
                     }}
                 >
                     <div className={styles.imageAndText}>
-                        <img src={starosc1} />
+                        <img alt="starość" src={starosc1} />
                         <div>
                             W 1954 r., jeden z naszych przodków, Pan Albert, zaczął z nudów rzeźbić w kawałku pnia kamieniem, po chwili dostrzegł w drzewie pewien tajemniczy metalowy przedmiot, zaczął
                             ku mu kopać i okazało się że to jakiś niewybuch, odciął sobie 2 palce.
@@ -82,7 +76,7 @@ function App() {
                         >
                             undefined
                         </div>
-                        <img src={obrobkaMetalu1} />
+                        <img alt="obróbka metalu" src={obrobkaMetalu1} />
                     </div>
                     <br />
                     <br />
