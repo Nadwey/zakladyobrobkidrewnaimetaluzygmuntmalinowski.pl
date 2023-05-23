061bfc4035c92190db053516ce6fcb461f8b9df9
Nadwey <81181783+Nadwey@users.noreply.github.com>
2023-05-23 06:08:42 +0000

    Update

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import { Opinia } from "./Opinia";
 import { Group } from "@mantine/core";
 
 // images
-import tnieDrewno1 from "./img/tnie-drewno-1.jpg";
+// import tnieDrewno1 from "./img/tnie-drewno-1.jpg";
 import tnieDrewno2 from "./img/tnie-drewno-2.jpg";
 import starosc1 from "./img/starosc-1.webp";
 import klient1 from "./img/klient-1.jpg";
 import klient2 from "./img/klient-2.jpg";
 import klient3 from "./img/klient-3.jpg";
+import klient4 from "./img/klient-4.png";
 import obrobkaMetalu1 from "./img/obrobka-metalu-1.jpeg";
 import obrobkaMetalu2 from "./img/obrobka-metalu-2.jpg";
 import obrobkaMetalu3 from "./img/obrobka-metalu-3.jpg";
 import obrobkaMetalu4 from "./img/obrobka-metalu-4.jpg";
+import pasja from "./img/pasja.jpg";
 
 function App() {
     const [embla, setEmbla] = useState(null);
@@ -34,9 +36,6 @@ function App() {
         <div>
             <div className={styles.carouselContainer}>
                 <Carousel loop getEmblaApi={setEmbla} draggable={false} withControls={false} height="100%" sx={{ flex: 1 }}>
-                    <Carousel.Slide>
-                        <img alt="tnie drewno" className={styles.carouselImage} src={tnieDrewno1} />
-                    </Carousel.Slide>
                     <Carousel.Slide>
                         <img alt="tnie drewno" className={styles.carouselImage} src={tnieDrewno2} />
                     </Carousel.Slide>
@@ -74,7 +73,7 @@ function App() {
                                 textAlign: "right",
                             }}
                         >
-                            undefined
+                            Metal
                         </div>
                         <img alt="obróbka metalu" src={obrobkaMetalu1} />
                     </div>
@@ -102,7 +101,61 @@ function App() {
                             avatar={klient3}
                             opis="Odkurzacz bardzo dobry, godny polecenia. Rzadko się zdarza żeby w tym przedziale cenowym kupić odkurzacz z taką ilością osprzętu. Posiada wszystkie końcówki do odkurzania kazdej powierzchni. Filtr Hepa 13 zapewnia w czasie pracy czyste powietrze bez kurzu, roztoczy i alergenów. Polecam"
                         />
+                        <Opinia
+                            avatar={klient4}
+                            opis="Jestem zachwycony jakością usług świadczonych przez zakłady zajmujące się obróbką metalu i drewna. Ich profesjonalizm i precyzja w pracy są niezwykłe. Obrabiając drewno i metal, potrafią stworzyć prawdziwe arcydzieła, które zachwycają swoją estetyką i funkcjonalnością."
+                        />
                     </Group>
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <div className={styles.vCenter}>
+                        <span
+                            style={{
+                                fontSize: "3rem",
+                                fontStyle: "italic",
+                            }}
+                        >
+                            "Tworzymy piękno w drewnie i stalowej doskonałości!"
+                        </span>
+                    </div>
+                    <img alt="pasja" src={pasja} />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <div className={styles.vCenter}>
+                        <span
+                            style={{
+                                fontSize: "3rem",
+                            }}
+                        >
+                            Usługi
+                        </span>
+                    </div>
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <ul>
+                        <li>Stuł - 30zł</li>
+                        <li>Dwupiętrowa łódź ze stalowymi skrzydłami - Łódź mieszkalna zbudowana z drewna i wyposażona w skrzydła ze stali nierdzewnej - 200 000 złotych.</li>
+                        <li>Skoda Superb Skoda Superb III 190 KM 4x4 DSG L&K - 110 000 złotych.</li>
+                    </ul>
+                    <div className={styles.vCenter}>
+                        <span
+                            style={{
+                                fontSize: "3rem",
+                            }}
+                        >
+                            Kontakt
+                        </span>
+                    </div>
+                    <ul>
+                        <li>Telefon - +48 155-DREWNO-METAL</li>
+                        <li>Email - zygmuntmalinowski@zakladyobrobkidrewnaimetaluzygmuntmalinowski.pl</li>
+                    </ul>
                 </div>
             </div>
         </div>
--- src/Opinia.jsx
+++ src/Opinia.jsx
@@ -2,13 +2,7 @@ import { Avatar, Text, Paper } from "@mantine/core";
 
 export function Opinia({ avatar, opis }) {
     return (
-        <Paper
-            radius="md"
-            withBorder
-            p="lg"
-            w={250}
-            h={350}
-        >
+        <Paper radius="md" withBorder p="lg" w={250} h={350}>
             <Avatar src={avatar} size={120} radius={120} mx="auto" />
             <Text ta="center" c="dimmed" fz="sm">
                 {opis}
